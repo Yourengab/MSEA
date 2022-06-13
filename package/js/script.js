39372dbd8560aa93a8df784f34c3aaab11b0837b
Yourengab <yourenw1@gmail.com>
2022-06-13 20:59:59 +0800

    collab

---
+++ package/js/script.js
@@ -0,0 +1,5 @@
+const filter = document.querySelector(".filter");
+const navFilter = document.querySelector(".filterNav");
+filter.addEventListener("click", function () {
+  navFilter.classList.toggle("show");
+});
